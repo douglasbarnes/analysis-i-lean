68b704b42f465fbae528822b9248f1f273303070
douglasbarnes <32173060+douglasbarnes@users.noreply.github.com>
2026-07-21 22:49:35 +0100

    Complete Numbers and Sets: NumbersAndSets.lean

--- NumbersAndSets.lean
+++ NumbersAndSets.lean
@@ -4,6 +4,7 @@ import NumbersAndSets.NumberTheory
 import NumbersAndSets.Combinatorics
 import NumbersAndSets.RealAndSequences
 import NumbersAndSets.Countability
+import NumbersAndSets.Checks
 
 /-!
 The Part IA Numbers and Sets course, formalised from
