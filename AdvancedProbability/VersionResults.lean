a01d39bab6b72ab46db6dde0aa2db13f227d3cf0
douglasbarnes <32173060+douglasbarnes@users.noreply.github.com>
2026-07-25 11:58:38 +0100

    Add process version consequences

---
+++ AdvancedProbability/VersionResults.lean
@@ -0,0 +1,39 @@
+import AdvancedProbability.ContinuousTime
+
+noncomputable section
+
+open scoped MeasureTheory
+open Filter MeasureTheory
+
+namespace AdvancedProbability
+
+universe u
+
+namespace IsVersion
+
+/-- Coordinatewise almost-sure equality of two process versions implies almost-sure equality of every
+finite-dimensional random vector. -/
+theorem finiteDimensionalDistribution_ae_eq {Ω : Type u} [MeasurableSpace Ω]
+    {P : Measure Ω} {X Y : ContinuousProcess Ω} (hXY : IsVersion P X Y)
+    (times : List ℝ≥0) :
+    FiniteDimensionalDistribution X times =ᵐ[P]
+      FiniteDimensionalDistribution Y times := by
+  have hall : ∀ᵐ ω ∂P, ∀ i : Fin times.length,
+      X (times.get i) ω = Y (times.get i) ω := by
+    rw [Filter.eventually_all]
+    intro i
+    exact hXY (times.get i)
+  filter_upwards [hall] with ω hω
+  funext i
+  exact hω i
+
+/-- A strongly adapted version of a martingale is again a martingale. -/
+theorem martingale {Ω : Type u} {mΩ : MeasurableSpace Ω} {P : @Measure Ω mΩ}
+    {ℱ : Filtration ℝ≥0 mΩ} {X Y : ContinuousProcess Ω}
+    (hX : Martingale X ℱ P) (hY : StronglyAdapted ℱ Y) (hXY : IsVersion P X Y) :
+    Martingale Y ℱ P :=
+  hX.congr hY hXY
+
+end IsVersion
+
+end AdvancedProbability
